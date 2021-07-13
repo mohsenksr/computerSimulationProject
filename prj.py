a4c43819892fee9291be2a78a544573a67cc0267
mohsenksr <mohsen.kassiri@live.com>
2021-07-13 18:25:30 +0430

    customers in system couting was completed

--- prj.py
+++ prj.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import math
 
 
-class Costumer:
+class Customer:
     def __init__(self, priority, arrivalTime, enduranceTime):
         self.priority = priority
         self.arrivalTime = arrivalTime
@@ -25,8 +25,8 @@ for i in range(n):
 
 reception_q = [[] for i in range(5)]
 queues = [[[] for i in range(5)]for j in range(n)]
-costumer_count = 0
-costumer_prority_count = [0] * 5
+customer_count = 0
+customer_prority_count = [0] * 5
 time = 0
 
 
@@ -39,52 +39,53 @@ reception_q_len = 0
 queues_len = [0] * n
 
 times = [0]
+customers_in_system_array = [0]
 reception_q_lens_array = [0]
 queues_lens_array = [[0] for i in range(5)]
 
-reception_client = None  # the costumer who is geting served
+reception_client = None  # the customer who is geting served
 reception_service_time = 0
 
-costumer_limit = 100000
+customer_limit = 100000
 is_empty = True
-while costumer_count < costumer_limit or not is_empty:
+while customer_count < customer_limit or not is_empty:
     # check for customers endurance in reception queue
     for q in reception_q:
-        for costumer in reversed(q):
-            if time - costumer.arrivalTime > costumer.enduranceTime:
-                spent_time += time - costumer.arrivalTime
-                priority_spent_time[costumer.priority] += time - \
-                    costumer.arrivalTime
-                wait_time += time - costumer.arrivalTime
-                priority_wait_time[costumer.priority] += time - \
-                    costumer.arrivalTime
+        for customer in reversed(q):
+            if time - customer.arrivalTime > customer.enduranceTime:
+                spent_time += time - customer.arrivalTime
+                priority_spent_time[customer.priority] += time - \
+                    customer.arrivalTime
+                wait_time += time - customer.arrivalTime
+                priority_wait_time[customer.priority] += time - \
+                    customer.arrivalTime
                 leave_count += 1
-                q.remove(costumer)
+                q.remove(customer)
 
     # check for customers endurance in main queues
     for i in queues:
         for q in i:
-            for costumer in reversed(q):
-                if time - costumer.arrivalTime > costumer.enduranceTime:
-                    spent_time += time - costumer.arrivalTime
-                    priority_spent_time[costumer.priority] += time - \
-                        costumer.arrivalTime
-                    wait_time += time - costumer.queueArrivalTime
-                    priority_wait_time[costumer.priority] += time - \
-                        costumer.queueArrivalTime
+            for customer in reversed(q):
+                if time - customer.arrivalTime > customer.enduranceTime:
+                    spent_time += time - customer.arrivalTime
+                    priority_spent_time[customer.priority] += time - \
+                        customer.arrivalTime
+                    wait_time += time - customer.queueArrivalTime
+                    priority_wait_time[customer.priority] += time - \
+                        customer.queueArrivalTime
                     leave_count += 1
-                    q.remove(costumer)
+                    q.remove(customer)
 
     # check for customers endurance in servers
     for i in servers:
-        for costumer in reversed(i):
-            if time - costumer[1].arrivalTime > costumer[1].enduranceTime:
-                spent_time += time - costumer[1].arrivalTime
-                priority_spent_time[costumer[1].priority] += time - \
-                    costumer[1].arrivalTime
+        for customer in reversed(i):
+            if time - customer[1].arrivalTime > customer[1].enduranceTime:
+                spent_time += time - customer[1].arrivalTime
+                priority_spent_time[customer[1].priority] += time - \
+                    customer[1].arrivalTime
                 leave_count += 1
                 idle_servers[servers.index(i)] += 1
-                i.remove(costumer)
+                i.remove(customer)
 
     # check for customers endurance in reception
     if reception_client != None and time - reception_client.arrivalTime > reception_client.enduranceTime:
@@ -96,31 +97,31 @@ while costumer_count < costumer_limit or not is_empty:
 
     # Arrivals :
     # customers arrival at that time
-    if costumer_count < costumer_limit:
+    if customer_count < customer_limit:
         quantity = np.random.poisson(landa)
-        a = costumer_limit - costumer_count
+        a = customer_limit - customer_count
         if a < quantity:
             quantity = a
-        costumer_count += quantity
+        customer_count += quantity
         for i in range(quantity):
             priority = np.random.uniform()  # for customer priority
-            # amount of time that costumer get tired
+            # amount of time that customer get tired
             endurance = np.random.exponential(alpha)
             if priority <= 0.5:
-                reception_q[0].append(Costumer(0, time, endurance))
-                costumer_prority_count[0] += 1
+                reception_q[0].append(Customer(0, time, endurance))
+                customer_prority_count[0] += 1
             elif priority <= 0.7:
-                reception_q[1].append(Costumer(1, time, endurance))
-                costumer_prority_count[1] += 1
+                reception_q[1].append(Customer(1, time, endurance))
+                customer_prority_count[1] += 1
             elif priority <= 0.85:
-                reception_q[2].append(Costumer(2, time, endurance))
-                costumer_prority_count[2] += 1
+                reception_q[2].append(Customer(2, time, endurance))
+                customer_prority_count[2] += 1
             elif priority <= 0.95:
-                reception_q[3].append(Costumer(3, time, endurance))
-                costumer_prority_count[3] += 1
+                reception_q[3].append(Customer(3, time, endurance))
+                customer_prority_count[3] += 1
             else:
-                reception_q[4].append(Costumer(4, time, endurance))
-                costumer_prority_count[4] += 1
+                reception_q[4].append(Customer(4, time, endurance))
+                customer_prority_count[4] += 1
 
     # reception service:
     if reception_client != None:
@@ -151,10 +152,10 @@ while costumer_count < costumer_limit or not is_empty:
             for j in reversed(range(len(servers[i]))):
                 servers[i][j][0] -= 1
                 if servers[i][j][0] == 0:
-                    costumer = servers[i].pop(j)[1]
-                    spent_time += time - costumer.arrivalTime
-                    priority_spent_time[costumer.priority] += time - \
-                        costumer.arrivalTime
+                    customer = servers[i].pop(j)[1]
+                    spent_time += time - customer.arrivalTime
+                    priority_spent_time[customer.priority] += time - \
+                        customer.arrivalTime
                     idle_servers[i] += 1
 
     # move from queues to servers
@@ -165,11 +166,11 @@ while costumer_count < costumer_limit or not is_empty:
                     idle_servers[i] -= 1
                     service_time = math.ceil(np.random.exponential(
                         1/operatorMioos[i][idle_servers[i]]))
-                    costumer = queues[i][k].pop(0)
-                    wait_time += time - costumer.queueArrivalTime
-                    priority_wait_time[costumer.priority] += time - \
-                        costumer.queueArrivalTime
-                    servers[i].append([service_time, costumer])
+                    customer = queues[i][k].pop(0)
+                    wait_time += time - customer.queueArrivalTime
+                    priority_wait_time[customer.priority] += time - \
+                        customer.queueArrivalTime
+                    servers[i].append([service_time, customer])
                     if idle_servers[i] == 0:
                         break
 
@@ -198,28 +199,36 @@ while costumer_count < costumer_limit or not is_empty:
                 else:
                     is_empty = True
 
-    q_len = sum([len(x) for x in reception_q])
-    reception_q_len += q_len
-    reception_q_lens_array.append(q_len)
+    temp_reception_q_len = sum([len(x) for x in reception_q])
+    reception_q_len += temp_reception_q_len
+    reception_q_lens_array.append(temp_reception_q_len)
+    comulative_queues_count = temp_reception_q_len
     for i in range(n):
         q_len = sum([len(x) for x in queues[i]])
+        comulative_queues_count += q_len
         queues_len[i] += q_len
         queues_lens_array[i].append(q_len)
+
     times.append(time)
+    customer_in_system = comulative_queues_count + \
+        sum([len(x) for x in servers])
+    if(reception_client != None):
+        customer_in_system += 1
+    customers_in_system_array.append(customer_in_system)
 
     time += 1
 
 
-print('Costumer count:\t', costumer_count)
-print('Tired costumer count:\t', leave_count)
-print('Avarage waiting time in queues:\t', wait_time / costumer_count)
+print('customer count:\t', customer_count)
+print('Tired customer count:\t', leave_count)
+print('Avarage waiting time in queues:\t', wait_time / customer_count)
 for i in range(5):
     print('\tAvarage waiting in queues of', i, 'priority costomers:\t',
-          priority_wait_time[i] / costumer_prority_count[i])
-print('Avarage spent time in the system:\t', spent_time / costumer_count)
+          priority_wait_time[i] / customer_prority_count[i])
+print('Avarage spent time in the system:\t', spent_time / customer_count)
 for i in range(5):
     print('\tAvarage spent time in the system of', i, 'priority costomers:\t',
-          priority_spent_time[i] / costumer_prority_count[i])
+          priority_spent_time[i] / customer_prority_count[i])
 print('Avarage reception queue length:\t', reception_q_len / time)
 for i in range(n):
     print('\tAvarage queue', i, 'length:\t', queues_len[i] / time)
@@ -243,3 +252,12 @@ for i in range(n):
     plt.title('length of queues in time')
     plt.legend()
     plt.show()
+
+y = customers_in_system_array
+plt.plot(x, y, label="customers in system")
+
+plt.xlabel('time')
+plt.ylabel('number')
+plt.title('customers in system in time')
+plt.legend()
+plt.show()
